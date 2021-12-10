c24cd903cfd6d28f205189dc02abe6220f2b479d
mithunbpalat <74177994+mithunbpalat@users.noreply.github.com>
2021-12-10 13:53:28 +0530

    Add files via upload

---
+++ ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.app.springsecurity;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBasicSecurityApplication.class);
+	}
+
+}
